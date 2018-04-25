cd2e75457669128e89577c2c538c7b543a931ea0
Jason Burmark <burmark1@llnl.gov>
2018-04-25 09:06:47 -0700

    Document batch_util batch/persistent kernel.

--- include/batch_utils.cuh
+++ include/batch_utils.cuh
@@ -345,13 +345,55 @@ inline const volatile device_wrapper_ptr* event_complete(const volatile device_w
   return (const volatile device_wrapper_ptr*)( event_ptr_ptr + 1 );
 }
 
+
+/*
+Implements a kernel that runs functions and events in buffer arg_buf.
+Reads arg_buf by creating a shared shared_buffer_type to copy parts of the buffer into shared memory.
+Moves through the shared buffer running functions and executing events.
+Reads more from arg_buf as necessary, and when complete switches to another, stopping when gets nullptr.
+Implements the following diagram.
+
+                    Start      Decide3 ---> Stop
+                      |       /    /\
+                     \/      \/     |
+                    Init  ReInit  BufFinalize
+                      |   |         /\
+                     \/  \/          |
+                    BufRead      GridSync
+                     | /\          /\
+                    \/  |           |
+EventFinish <-----> Decide1 --> EventStart
+ /\                      |        /\
+  |                     \/         |
+GridSync   GridSync -> Compute     |
+ /\               /\     |         |
+  |                \    \/         |
+EventStart <------- Decide2 -------|
+                      | /\
+                     \/  |
+                    BufRead
+
+This diagram represents an algorithm that always
+  does a GridSync between Computes
+      Compute ...> GridSync ...> Compute
+  does an EventStart and GridSync between a Compute and an EventFinish
+      Compute ...> EventStart ...> GridSync ...> EventFinish
+  does a GridSync between  BufRead and BufFinalize
+      BufRead ...> GridSync ...> BufFinalize
+  does exactly one GridSync per Compute between Init or Reinit and BufFinalize if one or more Compute
+    and does exactly one GridSync between Init or Reinit and BufFinalize if no Compute
+      Init|Reinit ...> numberof(GridSync) == max(numberof(Compute), 1) ...> BufFinalize
+
+*/
 template < typename shared_buffer_type >
 __global__
 void block_read_device(typename shared_buffer_type::buffer_type volatile* arg_buf)
 {
+  // Start:
   __shared__ shared_buffer_type volatile shr_buf;
   __shared__ typename shared_buffer_type::buffer_type volatile* volatile dev_buf;
 
+  // Init:
   if (threadIdx.x == 0) {
     dev_buf = arg_buf;
   }
@@ -365,44 +407,14 @@ void block_read_device(typename shared_buffer_type::buffer_type volatile* arg_bu
 
   pre_loop:
 
+  // Decide3:
   while (dev_buf != nullptr) {
 
     const volatile void* shr_ptr = shr_buf.buffer();
 
     device_wrapper_ptr fnc = nullptr;
 
-    // loop until get first function
-    while (1) {
-
-      // read in part of device buffer into shared buffer
-      shr_buf.read(dev_buf);
-
-      while (shr_ptr != shr_buf.buffer()) {
-
-        fnc = ((const volatile device_wrapper_ptr*)shr_ptr)[0];
-        /*
-        if (threadIdx.x+1 == blockDim.x) {
-          FPRINTF(stdout, "block %6i thread %6i read fnc %p\n", blockIdx.x, threadIdx.x, fnc);
-        }
-        */
-        if (fnc == comb_detail_fnc_event_val) {
-
-          // nothing done yet, so nothing to synchronize
-          // complete event located at shr_buf
-          shr_ptr = event_complete(&((const volatile device_wrapper_ptr*)shr_ptr)[1]);
-
-        } else if (fnc != comb_detail_fnc_null_val) {
-
-          goto do_fnc;
-
-        } else {
-
-          goto finish_buf;
-
-        }
-      }
-
-    }
+    goto synced_loop_read;
 
     // execute functions in read buffer
     while (1) {
@@ -415,12 +427,13 @@ void block_read_device(typename shared_buffer_type::buffer_type volatile* arg_bu
           FPRINTF(stdout, "block %6i thread %6i read fnc %p\n", blockIdx.x, threadIdx.x, fnc);
         }
         */
+        // Decide2:
         if (fnc == comb_detail_fnc_event_val) {
 
-          // ensure memory writes in all threads readable everywhere
+          // EventStart: ensure memory writes in all threads readable everywhere
           __threadfence_system();
 
-          // synchronize before complete event
+          // GridSync: synchronize before complete event
           { cooperative_groups::this_grid().sync(); }
 
           // complete event located at shr_buf
@@ -430,12 +443,13 @@ void block_read_device(typename shared_buffer_type::buffer_type volatile* arg_bu
 
             while (shr_ptr != shr_buf.buffer()) {
 
-              device_wrapper_ptr fnc = ((const volatile device_wrapper_ptr*)shr_ptr)[0];
+              fnc = ((const volatile device_wrapper_ptr*)shr_ptr)[0];
               /*
               if (threadIdx.x+1 == blockDim.x) {
                 FPRINTF(stdout, "block %6i thread %6i read fnc %p\n", blockIdx.x, threadIdx.x, fnc);
               }
               */
+              // Decide1:
               if (fnc == comb_detail_fnc_event_val) {
 
                 // repeat event, no need to synchronize
@@ -454,12 +468,12 @@ void block_read_device(typename shared_buffer_type::buffer_type volatile* arg_bu
 
             }
 
-            // read in part of device buffer into shared buffer
+            synced_loop_read:
+
+            // BufRead: read in part of device buffer into shared buffer
             shr_buf.read(dev_buf);
 
           }
-
-
 
         } else if (fnc != comb_detail_fnc_null_val) {
           /*
@@ -467,12 +481,12 @@ void block_read_device(typename shared_buffer_type::buffer_type volatile* arg_bu
             FPRINTF(stdout, "block %6i thread %6i grid sync\n", blockIdx.x, threadIdx.x);
           }
           */
-          // synchronize before run function
+          // GridSync: synchronize before run function
           { cooperative_groups::this_grid().sync(); }
 
           do_fnc:
 
-          // run function in this buffer
+          // Compute: run function in this buffer
           shr_ptr = fnc(&((const volatile device_wrapper_ptr*)shr_ptr)[1]);
 
         } else {
@@ -480,7 +494,7 @@ void block_read_device(typename shared_buffer_type::buffer_type volatile* arg_bu
           finish_buf:
 
           {
-            // finish of this buffer, get next buffer
+            // Reinit: finish of this buffer, get next buffer
             typename shared_buffer_type::buffer_type volatile* old_buf;
             if (threadIdx.x == 0) {
               // use thread 0 to avoid race condition when reading and writing dev_buf
@@ -492,13 +506,18 @@ void block_read_device(typename shared_buffer_type::buffer_type volatile* arg_bu
               FPRINTF(stdout, "block %6i thread %6i grid sync\n", blockIdx.x, threadIdx.x);
             }
             */
-            // ensure all blocks have read the end of buffer, next buffer
+            // EventStart: ensure memory writes in all threads readable everywhere
+            __threadfence_system();
+
+            // GridSync: ensure all blocks have read the end of buffer, next buffer
             { cooperative_groups::this_grid().sync(); }
 
+            // BufFinalize:
             if (blockIdx.x == 0) {
               // inform host that device is done reading this block
               shr_buf.done_reading(old_buf);
             }
+            // Reinit:
             shr_buf.reinit();
             /*
             if (threadIdx.x+1 == blockDim.x) {
@@ -520,12 +539,14 @@ void block_read_device(typename shared_buffer_type::buffer_type volatile* arg_bu
 
       }
 
-      // read in part of device buffer into shared buffer
+      // BufRead: read in part of device buffer into shared buffer
       shr_buf.read(dev_buf);
 
     }
 
   }
+
+  // Stop:
 }
 
 } // namespace detail
