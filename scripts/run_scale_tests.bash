715cb493efa2671bdb012ce479a49210955a217d
Jason Burmark <burmark1@llnl.gov>
2018-07-05 09:38:28 -0700

    Set core isolation num cores to 2

--- scripts/run_scale_tests.bash
+++ scripts/run_scale_tests.bash
@@ -7,7 +7,7 @@ procs_per_node=4
 let procs=procs_per_side*procs_per_side*procs_per_side
 let nodes=(procs+procs_per_node-1)/procs_per_node
 
-get_nodes="bsub -nnodes ${nodes} -core_isolation Y -W 60 -G guests -q pdebug -Is"
+get_nodes="bsub -nnodes ${nodes} -core_isolation 2 -W 60 -G guests -q pdebug -Is"
 run_tests="scale_tests.bash $nodes $procs $procs_per_side"
 
 full_test="${get_nodes} ${run_tests}"
