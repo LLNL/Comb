fda405e89b769f6c9cf51ca8a1585248af1f8369
Jason Burmark <burmark1@llnl.gov>
2019-06-20 10:29:21 -0700

    Implement gpump comm policy methods

--- include/comm_pol_gpump.hpp
+++ include/comm_pol_gpump.hpp
@@ -26,6 +26,115 @@
 #include "utils.hpp"
 #include "MessageBase.hpp"
 
+struct GpumpRequest
+{
+  int status;
+  struct gpump* comm;
+  int partner_rank;
+  ContextEnum context_type;
+  union context_union {
+    int invalid;
+    CPUContext cpu;
+    CudaContext cuda;
+    context_union() : invalid(-1) {}
+    ~context_union() {}
+  } context;
+
+  GpumpRequest()
+    : status(0)
+    , comm(nullptr)
+    , partner_rank(-1)
+    , context_type(ContextEnum::invalid)
+    , context()
+  {
+
+  }
+
+  GpumpRequest(GpumpRequest const& other)
+    : status(other.status)
+    , comm(other.comm)
+    , partner_rank(other.partner_rank)
+    , context_type(ContextEnum::invalid)
+    , context()
+  {
+    copy_context(other.context_type, other.context);
+  }
+
+  GpumpRequest& operator=(GpumpRequest const& other)
+  {
+    status = other.status;
+    comm = other.comm;
+    partner_rank = other.partner_rank;
+    copy_context(other.context_type, other.context);
+    return *this;
+  }
+
+  ~GpumpRequest()
+  {
+    destroy_context();
+  }
+
+  void setContext(CPUContext const& con)
+  {
+    if (context_type == ContextEnum::cpu) {
+      context.cpu = con;
+    } else {
+      destroy_context();
+      new(&context.cpu) CPUContext(con);
+      context_type = ContextEnum::cpu;
+    }
+  }
+
+  void setContext(CudaContext const& con)
+  {
+    if (context_type == ContextEnum::cuda) {
+      context.cuda = con;
+    } else {
+      destroy_context();
+      new(&context.cuda) CudaContext(con);
+      context_type = ContextEnum::cuda;
+    }
+  }
+
+private:
+  void copy_context(ContextEnum const& other_type, context_union const& other_context)
+  {
+    switch (other_type) {
+      case (ContextEnum::invalid):
+      {
+        // do nothing
+      } break;
+      case (ContextEnum::cpu):
+      {
+        setContext(other_context.cpu);
+      } break;
+      case (ContextEnum::cuda):
+      {
+        setContext(other_context.cuda);
+      } break;
+    }
+  }
+
+  void destroy_context()
+  {
+    switch (context_type) {
+      case (ContextEnum::invalid):
+      {
+        // do nothing
+      } break;
+      case (ContextEnum::cpu):
+      {
+        context.cpu.~CPUContext();
+      } break;
+      case (ContextEnum::cuda):
+      {
+        context.cuda.~CudaContext();
+      } break;
+    }
+    context_type = ContextEnum::invalid;
+  }
+};
+
 struct gpump_pol {
   // static const bool async = false;
   static const bool mock = false;
@@ -33,10 +142,10 @@ struct gpump_pol {
   using communicator_type = struct gpump*;
   static inline communicator_type communicator_create(MPI_Comm comm) { return gpump_init(comm); }
   static inline void communicator_destroy(communicator_type g) { gpump_term(g); }
-  using send_request_type = int;
-  static inline send_request_type send_request_null() { return 0; }
-  using recv_request_type = int;
-  static inline recv_request_type recv_request_null() { return 0; }
+  using send_request_type = GpumpRequest;
+  static inline send_request_type send_request_null() { return send_request_type{}; }
+  using recv_request_type = GpumpRequest;
+  static inline recv_request_type recv_request_null() { return recv_request_type{}; }
   using send_status_type = int;
   static inline send_status_type send_status_null() { return 0; }
   using recv_status_type = int;
@@ -132,15 +241,6 @@ struct Message<gpump_pol> : detail::MessageBase
   template < typename context >
   void unpack(context const& con, communicator_type comm)
   {
-
-
-    // TODO: know if waiting on stream or cpu
-
-    // gpump_stream_wait_recv_complete(comm, partner_rank(), con.stream());
-
-    // gpump_cpu_ack_recv(comm, partner_rank());
-
-
     static_assert(!std::is_same<context, ExecContext<mpi_type_pol>>::value, "gpump_pol does not support mpi_type_pol");
     DataT const* buf = m_buf;
     assert(buf != nullptr);
@@ -155,37 +255,44 @@ struct Message<gpump_pol> : detail::MessageBase
     }
   }
 
+private:
+  void start_Isend(CPUContext const&, communicator_type comm)
+  {
+    gpump_isend(comm, partner_rank(), m_region, 0, nbytes());
+    gpump_cpu_ack_isend(comm, partner_rank());
+  }
 
+  void start_Isend(CudaContext const& con, communicator_type comm)
+  {
+    gpump_stream_send(comm, partner_rank(), con.stream(), m_region, 0, nbytes());
+    gpump_stream_wait_send_complete(comm, partner_rank(), con.stream());
+  }
+
+public:
   template < typename context >
   void Isend(context const& con, communicator_type comm, send_request_type* request)
   {
     static_assert(!std::is_same<context, ExecContext<mpi_type_pol>>::value, "gpump_pol does not support mpi_type_pol");
     // FPRINTF(stdout, "%p Isend %p nbytes %d to %i tag %i\n", this, buffer(), nbytes(), partner_rank(), tag());
 
-
-    // TODO: know if waiting on stream or cpu
-
-    // gpump_stream_send(comm, partner_rank(), con.stream(), m_region, 0, nbytes());
-    // gpump_stream_wait_send_complete(comm, partner_rank(), con.stream());
-
-    // gpump_isend(comm, partner_rank(), m_region, 0, nbytes());
-    // gpump_cpu_ack_isend(comm, partner_rank());
-
-    // *request = 1;
-
-
+    start_Isend(con, comm);
+    request->status = 1;
+    request->comm = comm;
+    request->partner_rank = partner_rank();
+    request->setContext(con);
   }
 
   template < typename context >
-  void Irecv(context const&, communicator_type comm, recv_request_type* request)
+  void Irecv(context const& con, communicator_type comm, recv_request_type* request)
   {
     static_assert(!std::is_same<context, ExecContext<mpi_type_pol>>::value, "gpump_pol does not support mpi_type_pol");
     // FPRINTF(stdout, "%p Irecv %p nbytes %d to %i tag %i\n", this, buffer(), nbytes(), partner_rank(), tag());
 
-
-    // gpump_receive(comm, partner_rank(), m_region, 0, nbytes());
-    // *request = -1;
-
+    gpump_receive(comm, partner_rank(), m_region, 0, nbytes());
+    request->status = -1;
+    request->comm = comm;
+    request->partner_rank = partner_rank();
+    request->setContext(con);
   }
 
 
@@ -199,25 +306,29 @@ struct Message<gpump_pol> : detail::MessageBase
     }
   }
 
+private:
+  void wait_recv(CPUContext const&, communicator_type comm)
+  {
+    // already done
+  }
+
+  void wait_recv(CudaContext const& con, communicator_type comm)
+  {
+    gpump_wait_receive_complete(comm, partner_rank());
+  }
+
+public:
   template < typename context >
-  void deallocate(context const&, communicator_type comm, COMB::Allocator& buf_aloc)
+  void deallocate(context const& con, communicator_type comm, COMB::Allocator& buf_aloc)
   {
     static_assert(!std::is_same<context, ExecContext<mpi_type_pol>>::value, "gpump_pol does not support mpi_type_pol");
     if (m_buf != nullptr) {
 
-
-      // TODO: know where to wait for completion (don't block cpu)
-
-      // if (m_kind == Kind::send) {
-      //   gpump_wait_send_complete(comm, partner_rank());
-      //   gpump_is_send_complete(comm, partner_rank());
-      // } else if (m_kind == Kind::recv) {
-      //   gpump_wait_receive_complete(comm, partner_rank());
-      //   gpump_is_receive_complete(comm, partner_rank());
-      // }
-
-
-
+      if (m_kind == Kind::send) {
+        // already done
+      } else if (m_kind == Kind::recv) {
+        wait_recv(con, comm);
+      }
       gpump_deregister_region(comm, m_region);
       m_region = nullptr;
       buf_aloc.deallocate(m_buf);
@@ -226,27 +337,59 @@ struct Message<gpump_pol> : detail::MessageBase
   }
 
 
-  static int wait_send_any(int count, send_request_type* requests,
-                           send_status_type* statuses)
+private:
+  static bool start_wait_send(send_request_type& request)
   {
-    for (int i = 0; i < count; ++i) {
-      if (requests[i] != 2) {
-        assert(requests[i] == 1);
-        requests[i] = 2;
-        statuses[i] = 1;
-        return i;
-      }
+    if (request.context_type == ContextEnum::cuda) {
+      return gpump_is_send_complete(request.comm, request.partner_rank);
+    } else if (request.context_type == ContextEnum::cpu) {
+      return gpump_is_send_complete(request.comm, request.partner_rank);
+    } else {
+      assert(0);
     }
-    return -1;
+    return false;
   }
 
+  static bool test_waiting_send(send_request_type& request)
+  {
+    if (request.context_type == ContextEnum::cuda) {
+      return gpump_is_send_complete(request.comm, request.partner_rank);
+    } else if (request.context_type == ContextEnum::cpu) {
+      return gpump_is_send_complete(request.comm, request.partner_rank);
+    } else {
+      assert(0);
+    }
+    return false;
+  }
+
+public:
   static int test_send_any(int count, send_request_type* requests,
                            send_status_type* statuses)
   {
     for (int i = 0; i < count; ++i) {
-      if (requests[i] != 2) {
-        assert(requests[i] == 1);
-        requests[i] = 2;
+      if (requests[i].status != 3) {
+        if (requests[i].status == 1) {
+          // haven't seen this request yet, start it
+          if (start_wait_send(requests[i])) {
+            // done
+            requests[i].status = 3;
+          } else {
+            // test again later
+            requests[i].status = 2;
+            continue;
+          }
+        } else if (requests[i].status == 2) {
+          // have seen this one before, test it
+          if (test_waiting_send(requests[i])) {
+            // done
+            requests[i].status = 3;
+          } else {
+            // test again later
+            continue;
+          }
+        } else {
+          assert(0);
+        }
         statuses[i] = 1;
         return i;
       }
@@ -254,19 +397,14 @@ struct Message<gpump_pol> : detail::MessageBase
     return -1;
   }
 
-  static int wait_send_some(int count, send_request_type* requests,
-                            int* indices, send_status_type* statuses)
+  static int wait_send_any(int count, send_request_type* requests,
+                           send_status_type* statuses)
   {
-    int done = 0;
-    for (int i = 0; i < count; ++i) {
-      if (requests[i] != 2) {
-        assert(requests[i] == 1);
-        requests[i] = 2;
-        statuses[i] = 1;
-        indices[done++] = i;
-      }
-    }
-    return done;
+    int ready = -1;
+    do {
+      ready = test_send_any(count, requests, statuses);
+    } while (ready == -1);
+    return ready;
   }
 
   static int test_send_some(int count, send_request_type* requests,
@@ -274,9 +412,29 @@ struct Message<gpump_pol> : detail::MessageBase
   {
     int done = 0;
     for (int i = 0; i < count; ++i) {
-      if (requests[i] != 2) {
-        assert(requests[i] == 1);
-        requests[i] = 2;
+      if (requests[i].status != 3) {
+        if (requests[i].status == 1) {
+          // haven't seen this request yet, start it
+          if (start_wait_send(requests[i])) {
+            // done
+            requests[i].status = 3;
+          } else {
+            // test again later
+            requests[i].status = 2;
+            continue;
+          }
+        } else if (requests[i].status == 2) {
+          // have seen this one before, test it
+          if (test_waiting_send(requests[i])) {
+            // done
+            requests[i].status = 3;
+          } else {
+            // test again later
+            continue;
+          }
+        } else {
+          assert(0);
+        }
         statuses[i] = 1;
         indices[done++] = i;
       }
@@ -284,53 +442,116 @@ struct Message<gpump_pol> : detail::MessageBase
     return done;
   }
 
-  static void wait_send_all(int count, send_request_type* requests,
-                            send_status_type* statuses)
+  static int wait_send_some(int count, send_request_type* requests,
+                            int* indices, send_status_type* statuses)
   {
-    for (int i = 0; i < count; ++i) {
-      if (requests[i] != 2) {
-        assert(requests[i] == 1);
-        requests[i] = 2;
-        statuses[i] = 1;
-      }
-    }
+    int done = 0;
+    do {
+      done = test_send_some(count, requests, indices, statuses);
+    } while (done == 0);
+    return done;
   }
 
   static bool test_send_all(int count, send_request_type* requests,
                             send_status_type* statuses)
   {
+    int done = 0;
     for (int i = 0; i < count; ++i) {
-      if (requests[i] != 2) {
-        assert(requests[i] == 1);
-        requests[i] = 2;
+      if (requests[i].status != 3) {
+        if (requests[i].status == 1) {
+          // haven't seen this request yet, start it
+          if (start_wait_send(requests[i])) {
+            // done
+            requests[i].status = 3;
+          } else {
+            // test again later
+            requests[i].status = 2;
+            continue;
+          }
+        } else if (requests[i].status == 2) {
+          // have seen this one before, test it
+          if (test_waiting_send(requests[i])) {
+            // done
+            requests[i].status = 3;
+          } else {
+            // test again later
+            continue;
+          }
+        } else {
+          assert(0);
+        }
         statuses[i] = 1;
       }
+      done++;
     }
-    return true;
+    return done == count;
   }
 
-
-  static int wait_recv_any(int count, recv_request_type* requests,
-                           recv_status_type* statuses)
+  static void wait_send_all(int count, send_request_type* requests,
+                            send_status_type* statuses)
   {
-    for (int i = 0; i < count; ++i) {
-      if (requests[i] != -2) {
-        assert(requests[i] == -1);
-        requests[i] = -2;
-        statuses[i] = 1;
-        return i;
-      }
-    }
-    return -1;
+    bool done = false;
+    do {
+      done = test_send_all(count, requests, statuses);
+    } while (!done);
   }
 
+
+private:
+  static bool start_wait_recv(recv_request_type& request)
+  {
+    if (request.context_type == ContextEnum::cuda) {
+      gpump_stream_wait_recv_complete(request.comm, request.partner_rank, request.context.cuda.stream());
+      return true;
+    } else if (request.context_type == ContextEnum::cpu) {
+      gpump_cpu_ack_recv(request.comm, request.partner_rank);
+      return gpump_is_receive_complete(request.comm, request.partner_rank);
+    } else {
+      assert(0);
+    }
+    return false;
+  }
+
+  static bool test_waiting_recv(recv_request_type& request)
+  {
+    if (request.context_type == ContextEnum::cuda) {
+      assert(0);
+    } else if (request.context_type == ContextEnum::cpu) {
+      return gpump_is_receive_complete(request.comm, request.partner_rank);
+    } else {
+      assert(0);
+    }
+    return false;
+  }
+
+public:
   static int test_recv_any(int count, recv_request_type* requests,
                            recv_status_type* statuses)
   {
     for (int i = 0; i < count; ++i) {
-      if (requests[i] != -2) {
-        assert(requests[i] == -1);
-        requests[i] = -2;
+      if (requests[i].status != -3) {
+        if (requests[i].status == -1) {
+          // haven't seen this request yet, start it
+          if (start_wait_recv(requests[i])) {
+            // done
+            requests[i].status = -3;
+          } else {
+            // test again later
+            requests[i].status = -2;
+            continue;
+          }
+        } else if (requests[i].status == -2) {
+          // have seen this one before, test it
+          if (test_waiting_recv(requests[i])) {
+            // done
+            requests[i].status = -3;
+          } else {
+            // test again later
+            continue;
+          }
+        } else {
+          assert(0);
+        }
         statuses[i] = 1;
         return i;
       }
@@ -338,19 +559,14 @@ struct Message<gpump_pol> : detail::MessageBase
     return -1;
   }
 
-  static int wait_recv_some(int count, recv_request_type* requests,
-                            int* indices, recv_status_type* statuses)
+  static int wait_recv_any(int count, recv_request_type* requests,
+                           recv_status_type* statuses)
   {
-    int done = 0;
-    for (int i = 0; i < count; ++i) {
-      if (requests[i] != -2) {
-        assert(requests[i] == -1);
-        requests[i] = -2;
-        statuses[i] = 1;
-        indices[done++] = i;
-      }
-    }
-    return done;
+    int ready = -1;
+    do {
+      ready = test_recv_any(count, requests, statuses);
+    } while (ready == -1);
+    return ready;
   }
 
   static int test_recv_some(int count, recv_request_type* requests,
@@ -358,9 +574,29 @@ struct Message<gpump_pol> : detail::MessageBase
   {
     int done = 0;
     for (int i = 0; i < count; ++i) {
-      if (requests[i] != -2) {
-        assert(requests[i] == -1);
-        requests[i] = -2;
+      if (requests[i].status != -3) {
+        if (requests[i].status == -1) {
+          // haven't seen this request yet, start it
+          if (start_wait_recv(requests[i])) {
+            // done
+            requests[i].status = -3;
+          } else {
+            // test again later
+            requests[i].status = -2;
+            continue;
+          }
+        } else if (requests[i].status == -2) {
+          // have seen this one before, test it
+          if (test_waiting_recv(requests[i])) {
+            // done
+            requests[i].status = -3;
+          } else {
+            // test again later
+            continue;
+          }
+        } else {
+          assert(0);
+        }
         statuses[i] = 1;
         indices[done++] = i;
       }
@@ -368,29 +604,58 @@ struct Message<gpump_pol> : detail::MessageBase
     return done;
   }
 
-  static void wait_recv_all(int count, recv_request_type* requests,
-                            recv_status_type* statuses)
+  static int wait_recv_some(int count, recv_request_type* requests,
+                            int* indices, recv_status_type* statuses)
   {
-    for (int i = 0; i < count; ++i) {
-      if (requests[i] != -2) {
-        assert(requests[i] == -1);
-        requests[i] = -2;
-        statuses[i] = 1;
-      }
-    }
+    int done = 0;
+    do {
+      done = test_recv_some(count, requests, indices, statuses);
+    } while (done == 0);
+    return done;
   }
 
   static bool test_recv_all(int count, recv_request_type* requests,
                             recv_status_type* statuses)
   {
+    int done = 0;
     for (int i = 0; i < count; ++i) {
-      if (requests[i] != -2) {
-        assert(requests[i] == -1);
-        requests[i] = -2;
+      if (requests[i].status != -3) {
+        if (requests[i].status == -1) {
+          // haven't seen this request yet, start it
+          if (start_wait_recv(requests[i])) {
+            // done
+            requests[i].status = -3;
+          } else {
+            // test again later
+            requests[i].status = -2;
+            continue;
+          }
+        } else if (requests[i].status == -2) {
+          // have seen this one before, test it
+          if (test_waiting_recv(requests[i])) {
+            // done
+            requests[i].status = -3;
+          } else {
+            // test again later
+            continue;
+          }
+        } else {
+          assert(0);
+        }
         statuses[i] = 1;
       }
+      done++;
     }
-    return true;
+    return done == count;
+  }
+
+  static void wait_recv_all(int count, recv_request_type* requests,
+                            recv_status_type* statuses)
+  {
+    bool done = false;
+    do {
+      done = test_recv_all(count, requests, statuses);
+    } while (!done);
   }
 
 private:
